876ab300e46981de9a73746a166e90e3354a09ec
ana-rosales <anapaula161103@gmail.com>
2023-12-07 20:05:35 -0600

    FEAT: Duck entity added.

---
+++ Example/src/example/entities/Duck.java
@@ -0,0 +1,51 @@
+
+package example.entities;
+
+/**
+ *
+ * @author paula
+ */
+public class Duck {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Duck() {
+    }
+
+    public Duck(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Duck{" + "name=" + name + ", breed=" + breed + ", age=" + age + '}';
+    }
+    
+}
